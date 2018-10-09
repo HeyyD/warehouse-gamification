4585328ed0adeabf4a43173e8cc763edf6f52b08
HeyyD <heidi.hati@cs.tamk.fi>
2018-10-09 19:31:29 +0300

    Add canvas

--- front-end/warehouse-gamification/src/Avatar.tsx
+++ front-end/warehouse-gamification/src/Avatar.tsx
@@ -3,10 +3,30 @@ import * as React from 'react';
 import spritesheet from './assets/player_sprites.png'
 
 class Avatar extends React.Component {
+
+  private image = new Image()
+  private canvas: HTMLCanvasElement | null;
+  
+  constructor(props: any) {
+    super(props);
+    this.image.src = spritesheet;
+  }
+
+  public componentDidMount() {
+    this.updateCanvas()
+  }
+
   public render() {
     return(
-      <img src={ spritesheet }/>
+      <canvas ref={ canvas => (this.canvas = canvas) }>ASD</canvas>
     );
+  }
+
+  private updateCanvas() {
+    if(this.canvas) {
+      const ctx = this.canvas.getContext('2d');
+      ctx!.fillRect(0, 0, 100, 100)
+    }
   }
 }
 
