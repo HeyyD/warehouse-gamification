3769bd92bd884632720dc80b51acc2e9c7de7134
HeyyD <heidi.hati@cs.tamk.fi>
2018-10-11 20:53:54 +0300

    Change spritesheet

--- front-end/warehouse-gamification/src/Avatar/Avatar.tsx
+++ front-end/warehouse-gamification/src/Avatar/Avatar.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 
-import player from '../assets/female_sprites.png';
+import player from '../assets/player_sprites105x.png';
 import SpriteSheet from './SpriteSheet';
 
 class Avatar extends React.Component {
 
-  private image = new Image(5040, 90)
+  private image = new Image()
   private canvas: HTMLCanvasElement | null;
+  private spritesheet?: SpriteSheet;
   
   constructor(props: any) {
     super(props);
     this.image.src = player;
     this.image.onload = () => {
+      this.spritesheet = new SpriteSheet(this.image, 8, 10);
       this.updateCanvas();
     }
   }
