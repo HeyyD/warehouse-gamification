737d4fc04cd3d52b39300467d8e1aedd5f928d79
HeyyD <heidi.hati@cs.tamk.fi>
2018-10-14 13:21:10 +0300

    png name fix

--- sprites/create-sprites.py
+++ sprites/create-sprites.py
@@ -1,14 +1,48 @@
-import zipfile
-import os
+from PIL import Image
+import os, math, time, sys
 
-sprites_zip = zipfile.ZipFile('Sprites.zip', 'r')
-current_directory = ''
+directory = sys.argv[1]
+max_frames_row = 10.0
+frames = []
+tile_width = 0
+tile_height = 0
 
-for element in sprites_zip.namelist():
-  directory, filename = os.path.split(element)
+spritesheet_width = 0
+spritesheet_height = 0
 
-  if current_directory != directory:
-    current_directory = directory
-    print(directory)
+files = os.listdir(directory + "/")
+files.sort()
 
-sprites_zip.close()
+for current_file in files :
+    try:
+        with Image.open(directory + "/" + current_file) as im :
+            frames.append(im.getdata())
+    except:
+        print(current_file + " is not a valid image")
+
+tile_width = frames[0].size[0]
+tile_height = frames[0].size[1]
+
+if len(frames) > max_frames_row :
+    spritesheet_width = tile_width * max_frames_row
+    required_rows = math.ceil(len(frames)/max_frames_row)
+    spritesheet_height = tile_height * required_rows
+else:
+    spritesheet_width = tile_width*len(frames)
+    spritesheet_height = tile_height
+    
+spritesheet = Image.new("RGBA",(int(spritesheet_width), int(spritesheet_height)))
+
+for current_frame in frames :
+    top = tile_height * math.floor((frames.index(current_frame))/max_frames_row)
+    left = tile_width * (frames.index(current_frame) % max_frames_row)
+    bottom = top + tile_height
+    right = left + tile_width
+    
+    box = (left,top,right,bottom)
+    box = [int(i) for i in box]
+    cut_frame = current_frame.crop((0,0,tile_width,tile_height))
+    
+    spritesheet.paste(cut_frame, box)
+    
+spritesheet.save("spritesheet_" + directory + ".png", "PNG")
