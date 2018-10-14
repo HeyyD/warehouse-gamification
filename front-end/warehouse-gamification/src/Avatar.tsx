9194663be1ef685048c29ab58a67723641fe3246
HeyyD <heidi.hati@cs.tamk.fi>
2018-10-14 14:16:57 +0300

    Fix spite positioning issue

--- front-end/warehouse-gamification/src/Avatar.tsx
+++ front-end/warehouse-gamification/src/Avatar.tsx
@@ -7,6 +7,10 @@ import shirtImage from './assets/spritesheet_shirts.png';
 import skinImage from './assets/spritesheet_skin.png';
 import SpriteSheet from './SpriteSheet';
 
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class Avatar extends React.Component {
 
   private skinSrc = new Image();
@@ -38,7 +42,7 @@ class Avatar extends React.Component {
     return(
       <div className="avatar-container">
         <div className="canvas-container">
-          <canvas width={ 105 } height={ 105 } ref={ canvas => (this.canvas = canvas) }>The avatar canvas</canvas>
+          <canvas width={ 90 } height={ 90 } ref={ canvas => (this.canvas = canvas) }>The avatar canvas</canvas>
         </div>
         <button onClick={ this.updateCanvas }>Create random avatar</button>
       </div>
@@ -52,11 +56,16 @@ class Avatar extends React.Component {
       const shirt = this.shirt!;
       const hair = this.hair!;
 
-      ctx!.clearRect(0, 0, this.skinSrc.width / skin.getWidth(), skin.getHeight());
+      // ctx!.clearRect(0, 0, this.skinSrc.width / skin.getWidth(), skin.getHeight());
+      ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      skin.draw(ctx!, 0, 0);
-      shirt.draw(ctx!, 0, 0);
-      hair.draw(ctx!, 0, 0);
+      // skin.draw(ctx!, 0, 0);
+      // shirt.draw(ctx!, 0, 0);
+      // hair.draw(ctx!, 0, 0);
+
+      skin.draw(ctx!, getRandomInt(0, skin.getRows()), getRandomInt(0, skin.getColumns()));
+      shirt.draw(ctx!, getRandomInt(0, shirt.getRows()), getRandomInt(0, shirt.getColumns()));
+      hair.draw(ctx!, getRandomInt(0, hair.getRows()), getRandomInt(0, hair.getColumns()));
     }
   }
 }
