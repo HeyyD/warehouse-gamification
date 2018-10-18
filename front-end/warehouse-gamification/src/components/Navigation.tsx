7607583cd3c614614e37f095e35a1a97f07e6760
Sami <change@change.home>
2018-10-18 21:18:08 +0300

    Modify sidebarReducer functions

--- front-end/warehouse-gamification/src/components/Navigation.tsx
+++ front-end/warehouse-gamification/src/components/Navigation.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'; 
+import { connect } from 'react-redux';
+import { toggle } from '../reducers/sidebarReducer';
 import './Navigation.scss'; 
 import NavigationItem from './NavigationItem';
 
-const Navigation = () => {
+const Navigation = ({toggled, toggle} : {toggled: boolean, toggle: ()=>{}}) => {
   return (
     <div className='navigation'>
-      <i className='fa fa-bars' /> 
-      <div className='sidebar' style={{display: 'none'}}>
+      <i className='fa fa-bars' onClick={toggle}/> 
+      <div className='sidebar' style={{display: toggled? 'inline-block' : 'none'}}>
         <div className='sidebar-profile'>
           <img src='https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png' />
           <h2> Tom </h2>
@@ -19,4 +21,10 @@ const Navigation = () => {
   );
 };
 
-export default Navigation;
+const mapStateToProps = (state : {sidebar: boolean}) => {
+  return {
+    toggled: state.sidebar 
+  };
+};
+
+export default connect(mapStateToProps, {toggle})(Navigation);
--- front-end/warehouse-gamification/src/reducers/sidebarReducer.tsx
+++ front-end/warehouse-gamification/src/reducers/sidebarReducer.tsx
@@ -1,8 +1,12 @@
 
-const reducer = (state = true, action: {type: string}) => {
+const reducer = (state = false, action: {type: string}) => {
   switch(action.type){
     case 'TOGGLE':
       return !state;
+    case 'FALSE':
+      return false;
+    case 'TRUE':
+      return true;
     default:
       return state;
   }
@@ -12,6 +16,22 @@ export const toggle = () => {
   return async (dispatch: ({}) => {type: string}) => {
     dispatch({
       type: 'TOGGLE' 
+    });
+  };
+};
+
+export const setTrue = () => {
+  return async (dispatch: ({}) => {type: string}) => {
+    dispatch({
+      type: 'TRUE' 
+    });
+  };
+};
+
+export const setFalse = () => {
+  return async (dispatch: ({}) => {type: string}) => {
+    dispatch({
+      type: 'FALSE' 
     });
   };
 };
