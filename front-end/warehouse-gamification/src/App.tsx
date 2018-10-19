422bdc6c2cddffdce8a9fea8cfe2b6b77c5251ef
Sami <change@change.home>
2018-10-19 21:51:14 +0300

    Remove react-router

--- front-end/warehouse-gamification/src/App.tsx
+++ front-end/warehouse-gamification/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Route, Router } from 'react-router';
 import './App.scss';
 import MainLayout from './layouts/MainLayout'; 
 import MainPage from './pages/MainPage';
@@ -6,11 +7,14 @@ import MainPage from './pages/MainPage';
 class App extends React.Component {
   public render() {
     return (
-      <MainLayout>
-        <MainPage />
-      </MainLayout>
+      <div>
+      <Router>
+        <Route><MainLayout> <MainPage /> </MainLayout> </Route>
+      </Router>
+      </div>
     );
   }
 }
 
 export default App;
+
