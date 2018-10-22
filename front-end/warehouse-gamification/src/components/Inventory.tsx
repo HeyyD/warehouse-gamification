2dc215a5950d162c101fd4b72dae277a585d1f6f
HeyyD <heidi.hati@cs.tamk.fi>
2018-10-22 21:22:37 +0300

    base for router

--- front-end/warehouse-gamification/src/components/Inventory.tsx
+++ front-end/warehouse-gamification/src/components/Inventory.tsx
@@ -1,22 +1,30 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Item from './Item';
 
 import './Inventory.scss';
 
 class Inventory extends React.Component {
 
+  public items: JSX.Element[] = [];
+
   public render() {
 
-    const items = [];
     for (let i = 0; i < 20; i++) {
-      items.push(<Item/>);
+      this.items.push(<Item/>);
     }
 
     return (
       <div className='inventory-wrapper'>
-        <div className='inventory-container'>
-          { items }
-        </div>
+        <Router>
+          { 
+            // <Route path='/asd' render={ () => <div className='inventory-container'>{ items }</div> }/>
+          }
+          <React.Fragment>
+            <Route exact={ true } path='/inventory' render={ () => <Link to='/inventory/asd'>ASD</Link> }/>
+            <Route exact={ true } path='/inventory/asd' render={ () => <div className='inventory-container'>{ this.items }</div> }/>
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
