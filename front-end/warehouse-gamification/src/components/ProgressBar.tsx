5ba52d80a19d62fe1bc1e9a2d927ed5eb77275fb
Sami <change@change.home>
2018-10-31 17:10:51 +0200

    Modify ProgressBar renders Line

--- front-end/warehouse-gamification/src/components/ProgressBar.tsx
+++ front-end/warehouse-gamification/src/components/ProgressBar.tsx
@@ -1,8 +1,15 @@
+import { Line } from 'rc-progress';
 import * as React from 'react';
 
 const ProgressBar = () => {
   return(
-    <div> progressi baari</div> 
+    <Line 
+      strokeWidth={3}
+      trailWidth={3}
+      strokeColor="#eb9605"
+      trailColor="#d3d3d3"
+      percent={20}
+    />
   );
 };
 
