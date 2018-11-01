aadd4801e27ddf6b85627c57fffccf1193375017
Sami <change@change.home>
2018-11-01 09:41:22 +0200

    Modify Layout styles

--- front-end/warehouse-gamification/src/components/ProgressBar.tsx
+++ front-end/warehouse-gamification/src/components/ProgressBar.tsx
@@ -1,15 +1,18 @@
 import { Line } from 'rc-progress';
 import * as React from 'react';
+import './ProgressBar.scss';
 
 const ProgressBar = () => {
   return(
-    <Line 
-      strokeWidth={3}
-      trailWidth={3}
-      strokeColor="#eb9605"
-      trailColor="#d3d3d3"
-      percent={20}
-    />
+    <div className='progressbar'>
+      <Line 
+        strokeWidth={3}
+        trailWidth={3}
+        strokeColor="#eb9605"
+        trailColor="#d3d3d3"
+        percent={20}
+      />
+    </div>
   );
 };
 
--- front-end/warehouse-gamification/src/layouts/DesktopLayout.tsx
+++ front-end/warehouse-gamification/src/layouts/DesktopLayout.tsx
@@ -1,14 +1,40 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import Avatar from '../components/Avatar';
 import ProgressBar from '../components/ProgressBar';
+import Stats from '../components/Stats';
+import User from '../models/user';
+import './DesktopLayout.scss';
 
-const DesktopLayout = () => {
+const DesktopLayout = (props: {user: User}) => {
   return(
-    <div>
-      <Avatar /> 
-      <ProgressBar />
+    <div className='desktop-layout'>
+      <div className='upper-section'>
+        <Avatar />
+        <div className='upper-right-section'>
+          <ProgressBar />
+          <div className='player-info'>
+            <h3>Gladiator</h3>
+            <h2>Tom, lvl 22</h2>
+          </div>
+          <Stats user={props.user}/>
+        </div>
+      </div>
+      <div className='bottom-section'>
+        <div className='inventory-section'>
+          inventory
+        </div>
+        <div className='quest-section'>
+          quest
+        </div>
+      </div>
     </div>
   );
 };
+const mapStateToProps = (state: {user: User}) => {
+  return {
+    user: state.user 
+  };
+};
 
-export default DesktopLayout;
+export default connect(mapStateToProps)(DesktopLayout);
