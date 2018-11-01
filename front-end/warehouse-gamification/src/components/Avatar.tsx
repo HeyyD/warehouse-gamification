f03058e1da1f5126ed22f15704ea26e00c84347a
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-01 19:21:34 +0200

    refactor avatar

--- front-end/warehouse-gamification/src/components/Avatar.tsx
+++ front-end/warehouse-gamification/src/components/Avatar.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 
 import './Avatar.scss';
 
-import hairImage from '../assets/spritesheet_hair.png';
-import shirtImage from '../assets/spritesheet_shirts.png';
-import skinImage from '../assets/spritesheet_skin.png';
 import SpriteSheet from './SpriteSheet';
 import IUser from '../models/user';
 import { connect } from 'react-redux';
@@ -12,50 +9,37 @@ import IEquipment from '../models/IEquipment';
 
 interface IProps {
   equipment: IEquipment;
-  assets: {};
+  assets: {data: {}};
 }
 
 class Avatar extends React.Component<IProps> {
 
-  private skinSrc = new Image();
-  private shirtSrc = new Image();
-  private hairSrc = new Image();
-
-  private images = [
-    this.skinSrc,
-    this.shirtSrc,
-    this.hairSrc
-  ];
-
   private canvas: HTMLCanvasElement | null;
 
-  private skinSpritesheet?: SpriteSheet;
-  private shirtSpritesheet?: SpriteSheet;
-  private hairSpritesheet?: SpriteSheet;
-  
+  private skin: SpriteSheet;
+  private hair: SpriteSheet;
+  private shirt: SpriteSheet;
+
   constructor(props: IProps) {
     super(props);
-    this.skinSrc.src = skinImage;
-    this.shirtSrc.src = shirtImage;
-    this.hairSrc.src = hairImage;
+
+    this.reDraw = this.reDraw.bind(this);
+
+    const data = props.assets.data;
+    this.skin = data['skin'];
+    this.hair = data['hair'];
+    this.shirt = data['shirt'];
+  }
+
+  public componentDidMount() {
+    this.reDraw();
+  }
+
+  public componentDidUpdate() {
+    this.reDraw();
   }
 
   public render() {
-
-    if(this.canvas) {
-      const ctx = this.canvas.getContext('2d');
-      const skin = this.skinSpritesheet!;
-      const shirt = this.shirtSpritesheet!;
-      const hair = this.hairSpritesheet!;
-
-      ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      const equipment = this.props.equipment;
-
-      skin.draw(ctx!, skin.getSprite(equipment.skin));
-      shirt.draw(ctx!, shirt.getSprite(equipment.shirt));
-      hair.draw(ctx!, hair.getSprite(equipment.hair));
-    }
-
     return(
       <div className="avatar-container">
         <div className="canvas-container">
@@ -65,32 +49,17 @@ class Avatar extends React.Component<IProps> {
     );
   }
 
-  public componentDidMount() {
-    let loadedImages = 0;
-    this.images.forEach(img => {
-      img.onload = () => {
-        loadedImages++;
-        if(loadedImages === this.images.length) {
-          this.skinSpritesheet = new SpriteSheet(this.skinSrc, 13, 10, 6);
-          this.shirtSpritesheet = new SpriteSheet(this.shirtSrc, 4, 10, 6);
-          this.hairSpritesheet = new SpriteSheet(this.hairSrc, 16, 10, 4);
-
-          if(this.canvas) {
-            const ctx = this.canvas.getContext('2d');
-            const skin = this.skinSpritesheet!;
-            const shirt = this.shirtSpritesheet!;
-            const hair = this.hairSpritesheet!;
-      
-            ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            const equipment = this.props.equipment;
-
-            skin.draw(ctx!, skin.getSprite(equipment.skin));
-            shirt.draw(ctx!, shirt.getSprite(equipment.shirt));
-            hair.draw(ctx!, hair.getSprite(equipment.hair));
-          }
-        }
-      };
-    });
+  private reDraw() {
+    if(this.canvas) {
+      const ctx = this.canvas.getContext('2d');
+  
+      ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      const equipment = this.props.equipment;
+  
+      this.skin.draw(ctx!, this.skin.getSprite(equipment.skin));
+      this.shirt.draw(ctx!, this.shirt.getSprite(equipment.shirt));
+      this.hair.draw(ctx!, this.hair.getSprite(equipment.hair));
+    }
   }
 }
 
