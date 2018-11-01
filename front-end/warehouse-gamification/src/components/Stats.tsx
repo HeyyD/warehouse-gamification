d0d2d8aa24ca91c24ddac8be140bc624cdc4206d
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2018-11-01 12:39:33 +0200

    Add mobileReducer new reducer

--- front-end/warehouse-gamification/src/components/Stats.tsx
+++ front-end/warehouse-gamification/src/components/Stats.tsx
@@ -4,9 +4,10 @@ import Stat from './Stat';
 import './Stats.scss';
 
 const Stats = (props: {user: User}) => {
+  const isMobile = window.innerWidth <= 500;
   return (
     <div className='stats'>
-      <h2>Lifetime stats</h2>
+    {isMobile? <h2>Lifetime stats</h2> : null }
       <div className='stats-container'>
         <Stat icon='fa fa-archive' text='Boxes' stat={props.user.boxesPicked} />
         <Stat icon='fa fa-question' text='Quests' stat={props.user.questsCompleted} />
