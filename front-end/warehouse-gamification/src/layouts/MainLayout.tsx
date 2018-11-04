b39c1d83bfbaabb062cf938277e3ce0a5e3b29be
Sami <change@change.home>
2018-11-04 15:29:21 +0200

    Modify MainLayout add functionality

--- front-end/warehouse-gamification/src/layouts/MainLayout.tsx
+++ front-end/warehouse-gamification/src/layouts/MainLayout.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
+import DesktopLayout from './DesktopLayout';
+import { connect } from 'react-redux';
+import MobileLayout from './MobileLayout';
 
-const MainLayout = () => {
-  return <div> asdf </div>;
+
+const MainLayout = (props: { children: React.ReactNode, isMobile: boolean }) => {
+  return (
+    <React.Fragment>
+      {props.isMobile?
+      <MobileLayout >
+        {props.children}
+      </MobileLayout>
+      :
+      <DesktopLayout /> }
+    </React.Fragment>
+  );
 };
 
-export default MainLayout;
+const mapStateToProps = (state: {isMobile: boolean}) => {
+  return {
+    isMobile: state.isMobile 
+  };
+};
+export default connect(mapStateToProps)( MainLayout );
