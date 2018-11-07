d65846692535e6a77c94f31d310db86c1fe805f9
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-11-07 20:39:31 +0200

    Clean up routes

--- server/routes/quests.js
+++ server/routes/quests.js
@@ -11,4 +11,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+  if(isValidId(req.params.id)) {
+    models.Quest.findById(req.params.id)
+    .then(quests => {
+      res.json(quests);
+    })
+    .catch(err => {
+      res.json(err)
+    });
+  } else {
+    res.json({
+        msg: "Bad request"
+    })
+  }
+});
+
+
+const isValidId = (id) => {
+  if (!isNaN(id) & isFinite(id) & id %1 === 0) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = router;
