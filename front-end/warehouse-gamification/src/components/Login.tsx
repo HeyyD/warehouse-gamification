314b4e87cd96ad23d59c3ed902741ae1b1208f3d
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-08 14:02:58 +0200

    Login html

--- front-end/warehouse-gamification/src/components/Login.tsx
+++ front-end/warehouse-gamification/src/components/Login.tsx
@@ -3,7 +3,15 @@ import * as React from 'react';
 class Login extends React.Component {
   public render() {
     return (
-      <div>LOGIN</div>
+      <div>
+        <div>
+          <h1>Welcome</h1>
+          <p>Sign in with your crendentials</p>
+          <input type='text'/>
+          <input type='password'/>
+          <button>Login</button>
+        </div>
+      </div>
     );
   }
 }
