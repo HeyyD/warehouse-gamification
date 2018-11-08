5bfcd8675cd3408e5495b1697ca9f5449116aff2
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-08 22:22:09 +0200

    set state in login

--- front-end/src/components/Login.tsx
+++ front-end/src/components/Login.tsx
@@ -6,7 +6,40 @@ interface IProps {
   login: () => any;
 }
 
-class Login extends React.Component<IProps> {
+interface IState {
+  isReady: boolean;
+  username: string;
+  password: string;
+  users: [];
+}
+
+class Login extends React.Component<IProps, IState> {
+
+  constructor(props: IProps) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.login = this.login.bind(this);
+
+    this.state = {
+      isReady: false,
+      username: '',
+      password: '',
+      users: []
+    };
+  }
+
+  public componentDidMount() {
+    fetch('http://localhost:3001/api/users')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          isReady: true,
+          users: res
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
   public render() {
     return (
       <div className='login-container'>
@@ -14,13 +47,16 @@ class Login extends React.Component<IProps> {
           <h1>Welcome</h1>
           <p>Sign in with your credentials</p>
           <div className='input-container'>
-            <input type='text'/>
-            <input type='password'/>
+            <input name='username' type='text' onChange={e => this.onChange(e)}/>
+            <input name='password' type='password' onChange={e => this.onChange(e)}/>
           </div>
-          <button onClick={this.props.login}>Login</button>
+          <button disabled={!this.state.isReady} onClick={e => this.login(e)}>Login</button>
         </div>
       </div>
     );
+  }
+  private onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ [event.target.name]: event.target.value } as any);
   }
 }
 export default Login;
