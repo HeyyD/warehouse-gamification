6e062462cbee306c7e9c9f6077a91659f31973fd
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-11-10 20:54:56 +0200

    Add cors supports

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const app = express();
 
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 
 // Define routes
