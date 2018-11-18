080ea99e5396455f441e24ef03234a5dbc3af035
HeyyD <heidi.hati@cs.tamk.fi>
2018-11-18 13:44:02 +0200

    List all users

--- front-end/src/components/Friends.tsx
+++ front-end/src/components/Friends.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
 
-class Friends extends React.Component {
+interface IState {
+  users: JSX.Element[];
+}
+
+class Friends extends React.Component<{}, IState> {
+
+  constructor(props: {}) {
+    super(props);
+    this.createListItems = this.createListItems.bind(this);
+    this.state = {
+      users: []
+    };
+  }
+
+  public componentDidMount() {
+    fetch('http://localhost:3001/api/users')
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ users: this.createListItems(res)});
+    })
+    .catch(error => console.log(error));
+  }
+
   public render() {
     return(
-      <div>Friends working</div>
+      <ul>{this.state.users}</ul>
     );
+  }
+
+  private createListItems(users: Array<{username: string}>): JSX.Element[] {
+    return users.map((user, index) => {
+      return <li key={index}>{user.username}</li>;
+    });
   }
 }
 export default Friends;
