19af478fb0aeee433a724a2739c4eb660652a580
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-12-10 02:46:01 +0200

    Fix quests api address

--- front-end/src/services/questsService.tsx
+++ front-end/src/services/questsService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/quests';
+const baseUrl = '/api/quests';
 
 const getAll = async () => {
   const req = await axios.get(baseUrl);
